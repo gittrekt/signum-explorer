b5906c84016ee2013bdbc9047f2377d86a94f86f
gittrekt <76663968+gittrekt@users.noreply.github.com>
2023-08-30 18:38:53 +0000

    gunicorn use async worker if on

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,11 @@
 # https://docs.gunicorn.org/en/stable/settings.html
-
-import multiprocessing
+import multiprocessing, os
+from dotenv import load_dotenv
+load_dotenv()
 
 bind = "0.0.0.0:5000"
 workers = 2 * multiprocessing.cpu_count() - 1
 threads = 2 * multiprocessing.cpu_count()
-worker_class = 'config.workers.UvicornWorker'
 timeout = 60
 keepalive = 2
 max_requests = 500
@@ -14,3 +14,6 @@ worker_tmp_dir = "/dev/shm"
 forwarded_allow_ips = "*"
 proxy_allow_ips = "*"
 loglevel = "info"
+
+if os.environ.get("APP_ASYNC", "off") == "on":
+    worker_class = 'config.workers.UvicornWorker'
